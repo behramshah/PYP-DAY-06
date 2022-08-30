9e22f57be9a94c3c920a379fb6b32e9a94a3d346
Bahram <baxxxrrram@yandex.ru>
2022-08-30 22:03:05 +0400

    startstop watch

--- stop-watch/script.js
+++ stop-watch/script.js
@@ -2,28 +2,57 @@ const startButton = document.getElementById('start-button');
 const stopButton = document.getElementById('stop-button');
 const resetButton = document.getElementById('reset-button');
 const timer = document.getElementById('timer');
+const stopwatch = { elapsedTime: 0 }
 
-function enableButtons () {
+function onClickStart () {
     stopButton.removeAttribute('disabled');
     resetButton.removeAttribute('disabled');
+    startButton.setAttribute('disabled', true)
 }
 
-let isCounting = false;
-let time = 0;
-let timerID;
+function onClickStop () {
+  stopButton.setAttribute('disabled', true);
+  resetButton.removeAttribute('disabled');
+  startButton.removeAttribute('disabled');
+}
 
-function countUp() {
-    time++;
-    let min = Math.floor(time / 100 / 60);
-    let sec = Math.floor(time / 100);
-    if (sec >= 60) sec = sec % 60;
-    let milSec = Math.floor(time % 100);
-  
-    let timeStr = `${min > 9 ? min : "0" + min}:${sec > 9 ? sec : "0" + sec}:${
-      milSec > 9 ? milSec : "0" + milSec
-    }`;
-  
-    timeDisplay.textContent = timeStr;
-  }
+function onClickReset () {
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+  resetButton.setAttribute('disabled', true);
+}
 
-startButton.addEventListener('click', startTimer);
+function startStopwatch() {  
+  stopwatch.startTime = Date.now();
+  stopwatch.intervalId = setInterval(() => {
+    const elapsedTime = Date.now() - stopwatch.startTime + stopwatch.elapsedTime
+    const milliseconds = parseInt((elapsedTime%1000)/10)
+    const seconds = parseInt((elapsedTime/1000)%60)
+    const minutes = parseInt((elapsedTime/(1000*60))%60)
+    const hour = parseInt((elapsedTime/(1000*60*60))%24);
+    displayTime(hour, minutes, seconds, milliseconds)
+  }, 100);
+}
+
+function displayTime(hour, minutes, seconds, milliseconds) {
+  const leadZeroTime = [hour, minutes, seconds, milliseconds].map(time => time < 10 ? `0${time}` : time)
+  timer.innerHTML = leadZeroTime.join(':')
+}
+
+startButton.addEventListener('click', () => {
+  onClickStart();
+  startStopwatch();
+});
+
+stopButton.addEventListener('click', () => {
+  onClickStop();   
+  stopwatch.elapsedTime += Date.now() - stopwatch.startTime
+  clearInterval(stopwatch.intervalId)
+});
+
+resetButton.addEventListener('click', () => {
+  onClickReset();
+  stopwatch.elapsedTime = 0
+  stopwatch.startTime = Date.now()
+  displayTime(0, 0, 0, 0)
+})
